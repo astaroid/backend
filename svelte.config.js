1ce35005c8e772bbcad2e1fcf1995f67147ce2cd
daniel-rif <danielonyenwee@gmail.com>
2022-05-25 19:43:47 +0100

    ➕ add "hook" file and "lib" directory path

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import { resolve } from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,11 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		files: {
+			lib: resolve("./src/lib"),
+			hooks: resolve("./src/hooks.ts")
+		}
 	}
 };
 
