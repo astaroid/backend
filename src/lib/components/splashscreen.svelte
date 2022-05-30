470818cb518c7e032e4c376236259ef5d263feb2
daniel-rif <danielonyenwee@gmail.com>
2022-05-30 02:27:40 +0100

    ➕ add the component splashscreen

---
+++ src/lib/components/splashscreen.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let theme:"system"|"dark"|"light" = "system"
+</script>
+<main 
+    class="splash-screen" 
+    id={ theme == "dark" ? "dark" :  theme == "light" ? "light" : void 0 } 
+>
+    
+</main>
