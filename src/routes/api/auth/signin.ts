e0c1f08106729de798f479ab21240798139d2e2d
daniel-rif <danielonyenwee@gmail.com>
2022-06-02 02:10:15 +0100

    ➕ add security check to the "signin" and "signup" apis through the use of a "api_key"

--- src/routes/api/auth/signin.ts
+++ src/routes/api/auth/signin.ts
@@ -2,26 +2,42 @@ import type { RequestHandlerOutput } from "@sveltejs/kit"
 import type { RequestEvent } from "@sveltejs/kit/types/internal"
 import supabaseClient from "$lib/utils/supabaseClient"
 
-export async function post(event:RequestEvent): Promise<RequestHandlerOutput> {
-    let { 
-        usernameOrEmail, 
-        password, 
-    } = await event.request.json() as { usernameOrEmail:string, password:string }
+const {
+    VITE_API_KEY
+} = import.meta.env
 
-    const { data } = await supabaseClient
-        .from("users")
-        .select("password, id, email_confirm")
-        .or(`username.eq.${usernameOrEmail}, email.eq.${usernameOrEmail}`)
-        .single()
+export async function post({ url, request }:RequestEvent): Promise<RequestHandlerOutput> {
+    let apiKey = url.searchParams.get("api_key")
+
+    if (apiKey == VITE_API_KEY) {
+        let { 
+            usernameOrEmail, 
+            password, 
+        } = await request.json() as { usernameOrEmail:string, password:string }
     
-    if (data) {
-        if (password == data.password) {
-            return {
-                status: 200,
-                body: {
-                    user: {
-                        id: data.id,
-                        verify: data.email_confirm
+        const { data } = await supabaseClient
+            .from("users")
+            .select("password, id, email_confirm")
+            .or(`username.eq.${usernameOrEmail}, email.eq.${usernameOrEmail}`)
+            .single()
+        
+        if (data) {
+            if (password == data.password) {
+                return {
+                    status: 200,
+                    body: {
+                        user: {
+                            id: data.id,
+                            verify: data.email_confirm
+                        }
+                    }
+                }
+            } else {
+                return {
+                    status: 400,
+                    body: {
+                        code: 400,
+                        message: "Incorrect password"
                     }
                 }
             }
@@ -30,7 +46,7 @@ export async function post(event:RequestEvent): Promise<RequestHandlerOutput> {
                 status: 400,
                 body: {
                     code: 400,
-                    message: "Incorrect password"
+                    message: "Username or email doesn't exist"
                 }
             }
         }
@@ -39,8 +55,9 @@ export async function post(event:RequestEvent): Promise<RequestHandlerOutput> {
             status: 400,
             body: {
                 code: 400,
-                message: "Username or email doesn't exist"
+                message: "Incorrect api key"
             }
         }
     }
+    
 }
--- src/routes/api/auth/signup.ts
+++ src/routes/api/auth/signup.ts
@@ -2,86 +2,103 @@ import type { RequestHandlerOutput } from "@sveltejs/kit"
 import type { RequestEvent } from "@sveltejs/kit/types/internal"
 import supabaseClient from "$lib/utils/supabaseClient"
 
-export async function post(event:RequestEvent): Promise<RequestHandlerOutput> {
-    let { 
-        email, 
-        password, 
-        username 
-    } = await event.request.json() as { email:string, username:string, password:string }
+const {
+    VITE_API_KEY
+} = import.meta.env
 
-    let isEmailVerify = true, 
-        isUsernameVerify = true
+export async function post({ request, url }:RequestEvent): Promise<RequestHandlerOutput> {
+    let apiKey = url.searchParams.get("api_key")
+
+    if (apiKey == VITE_API_KEY) {
+        let { 
+            email, 
+            password, 
+            username 
+        } = await request.json() as { email:string, username:string, password:string }
     
-    let supabaseEmailVerify = await supabaseClient
-        .from("users")
-        .select("email")
-        .eq("email", email)
-        .single()
-    if (supabaseEmailVerify.data) {
-        isEmailVerify = false
-        return {
-            status: 400,
-            body: {
-                code: 400,
-                message: "Email already exist"
-            }
-        }
-    }
-
-    let supabaseUsernameVerify = await supabaseClient
-        .from("users")
-        .select("username")
-        .eq("username", username)
-        .single()
-    if (supabaseUsernameVerify.data) {
-        isUsernameVerify = false
-        return {
-            status: 400,
-            body: {
-                code: 400,
-                message: "Username already exist"
-            }
-        }
-    }
-
-    if (isEmailVerify && isUsernameVerify) {
-        const { user } = await supabaseClient.auth.api.createUser({
-            email_confirm: true,
-            email,
-            password,
-            user_metadata: {
-                username,
-                password,
-                email_confirm: false
-            }
-        })
-
-        if (user) {
-            return {
-                status: 200,
-                body: {
-                    user: {
-                        id: user.id,
-                        verify: user.user_metadata.email_confirm
-                    }
-                }
-            }
-        } else {
+        let isEmailVerify = true, 
+            isUsernameVerify = true
+        
+        let supabaseEmailVerify = await supabaseClient
+            .from("users")
+            .select("email")
+            .eq("email", email)
+            .single()
+        if (supabaseEmailVerify.data) {
+            isEmailVerify = false
             return {
                 status: 400,
                 body: {
                     code: 400,
-                    message: "Backend error"
+                    message: "Email already exist"
                 }
             }
         }
-    }
-
-    return {
-        status: 400,
-        body: {
-            code: 400,
-            message: "Backend error"
+    
+        let supabaseUsernameVerify = await supabaseClient
+            .from("users")
+            .select("username")
+            .eq("username", username)
+            .single()
+        if (supabaseUsernameVerify.data) {
+            isUsernameVerify = false
+            return {
+                status: 400,
+                body: {
+                    code: 400,
+                    message: "Username already exist"
+                }
+            }
+        }
+    
+        if (isEmailVerify && isUsernameVerify) {
+            const { user } = await supabaseClient.auth.api.createUser({
+                email_confirm: true,
+                email,
+                password,
+                user_metadata: {
+                    username,
+                    password,
+                    email_confirm: false
+                }
+            })
+    
+            if (user) {
+                return {
+                    status: 200,
+                    body: {
+                        user: {
+                            id: user.id,
+                            verify: user.user_metadata.email_confirm
+                        }
+                    }
+                }
+            } else {
+                return {
+                    status: 400,
+                    body: {
+                        code: 400,
+                        message: "Backend error"
+                    }
+                }
+            }
+        }
+    
+        return {
+            status: 400,
+            body: {
+                code: 400,
+                message: "Backend error"
+            }
+        }
+    } else {
+        return {
+            status: 400,
+            body: {
+                code: 400,
+                message: "Incorrect api key"
+            }
         }
     }
+    
 }
