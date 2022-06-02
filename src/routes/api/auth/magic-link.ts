a98053242b97bf2a0eb150d1175c64e6875fb4d9
daniel-rif <danielonyenwee@gmail.com>
2022-06-02 02:09:35 +0100

    ➕ add security check to the "magic-link" api through the use of a "api_key"

--- src/routes/api/auth/magic-link.ts
+++ src/routes/api/auth/magic-link.ts
@@ -2,25 +2,42 @@ import type { RequestHandlerOutput } from "@sveltejs/kit"
 import type { RequestEvent } from "@sveltejs/kit/types/internal"
 import supabaseClient from "$lib/utils/supabaseClient"
 
-export async function post(event:RequestEvent): Promise<RequestHandlerOutput> { 
-    let { email } = await event.request.json() as { email:string }
-    const { data } = await supabaseClient
-        .from("users")
-        .select("id")
-        .eq("email", email)
-        .single()
-    if (data) {
-        await supabaseClient.auth.api.sendMagicLinkEmail(email)
-        return {
-            status: 200
+const {
+    VITE_API_KEY
+} = import.meta.env
+
+export async function post({ url, request }:RequestEvent): Promise<RequestHandlerOutput> { 
+    let apiKey = url.searchParams.get("api_key")
+
+    if (apiKey == VITE_API_KEY) {
+        let { email } = await request.json() as { email:string }
+        const { data } = await supabaseClient
+            .from("users")
+            .select("id")
+            .eq("email", email)
+            .single()
+        if (data) {
+            await supabaseClient.auth.api.sendMagicLinkEmail(email)
+            return {
+                status: 200
+            }
+        } else {
+            return {
+                status: 400,
+                body: {
+                    code: 400,
+                    message: "Email doesn't exist"
+                }
+            }
         }
     } else {
         return {
             status: 400,
             body: {
                 code: 400,
-                message: "Email doesn't exist"
+                message: "Incorrect api key"
             }
         }
     }
+    
 }
