7f41a31ce91e0dd8c2442120490229a68f00a2bc
daniel-rif <danielonyenwee@gmail.com>
2022-08-07 13:22:04 +0100

    ➕ add error page for 404 requests

---
+++ src/routes/__error.svelte
@@ -0,0 +1,22 @@
+<script context="module" lang="ts">
+    /** @type {import('@sveltejs/kit').Load} */
+    export async function load(e:any) {
+      return {
+        props: {
+            status: e.status,
+            path: e.url.pathname,
+        }
+      };
+    }
+</script>
+
+<script lang="ts">
+    export let status:number
+    
+    export let path:string
+
+</script>
+
+<div style="margin: 15px; font-size: 15px">
+    <code>{status} - {path} not found</code>
+</div>
